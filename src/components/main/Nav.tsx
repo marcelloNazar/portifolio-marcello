0bd6b9c2aca7d094982933c04c529a08e5bd61ab
Marcello Nazar <marcellonazarb@hotmail.com>
2023-11-01 18:40:48 -0300

    atualizando

--- src/components/main/Nav.tsx
+++ src/components/main/Nav.tsx
@@ -8,29 +8,25 @@ import {
   HiEnvelope,
 } from "react-icons/hi2";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export const navData = [
-  { name: "inicio", path: "", icon: <HiHome /> },
-  {
-    name: "projetos",
-    path: "projects",
-    icon: <HiViewColumns />,
-  },
-  {
-    name: "sobre",
-    path: "about",
-    icon: <HiUser />,
-  },
+  { name: "inicio", path: "/", icon: <HiHome /> },
+  { name: "projetos", path: "/projects", icon: <HiViewColumns /> },
+  { name: "sobre", path: "/about", icon: <HiUser /> },
   {
     name: "contato",
-    path: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
 const Nav = () => {
   const pathName = usePathname();
+  const router = useRouter();
+  function handleClick(path: string) {
+    router.push(path);
+  }
   return (
     <nav
       className="absolute flex flex-col items-center xl:justify-center gap-y-4 h-max bottom-0 mt-auto 
@@ -47,6 +43,7 @@ const Nav = () => {
                 link.path === pathName && " bg-accent/30 rounded-full p-1"
               } relative flex items-center group hover:text-accent hover:scale-110 transition-all duration-300`}
               key={index}
+              onClick={() => handleClick(link.path)}
               href={link.path}
             >
               <div className="absolute h-10 w-[104px] left-1/2 transform -translate-x-1/2 bottom-[44px] hidden xl:group-hover:flex">
